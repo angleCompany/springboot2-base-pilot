063420e765ae7b204f6ed0557239a2fbaf4a5669
yiseungju <sjuyi@smilegate.com>
2019-05-17 16:32:42 +0900

    - Swagger API 적용

--- src/main/java/com/rest/api/controller/v1/UserController.java
+++ src/main/java/com/rest/api/controller/v1/UserController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 import com.rest.api.entity.User;
 import com.rest.api.repo.UserJpaRepo;
 
+import io.swagger.annotations.*;
+
+@Api(tags = {"1. User"})
 //@RequiredArgsConstructor ..//class상단에 선언하면 class내부에 final로 선언된 객체에 대해서 Constructor Injection을 수행합니다. 해당 어노테이션을 사용하지 않고 선언된 객체에 @Autowired를 사용해도 됩니다.
 @RestController // 결과 데이터를 JSON으로 내보냅니다.
 @RequestMapping(value = "/v1")
@@ -15,16 +18,21 @@ public class UserController  {
     @Autowired
     UserJpaRepo userJpaRepo;
 
+    @ApiOperation(value = "회원조회", notes = "모든 회원을 조회한다.")
     @GetMapping(value = "/user")
     public List<User> findAllUser() {
         return userJpaRepo.findAll();
     }
 
+    @ApiOperation(value = "회원 입력", notes = "회원을 입력한다.")
     @PostMapping(value = "/user")
-    public User save() {
+    public User save(@ApiParam(value = "회원 아이디", required = true)
+                         @RequestParam String uid,
+                     @ApiParam(value = "회원 이름", required = true)
+                         @RequestParam String name) {
         User user = User.builder()
-                .uid("zuurcode@gmail.com")
-                .name("이승주")
+                .uid(uid)
+                .name(name)
                 .build();
         return userJpaRepo.save(user);
 
